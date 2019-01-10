68100d4bc6dc1d3b4bb56c073e4facf2655af05f
Alexander Dovzhikov <alexander.dovzhikov@gmail.com>
2019-01-10 22:08:41 +1100

    Dates addition and subtraction

--- include/quantnum/time/boost_date.h
+++ include/quantnum/time/boost_date.h
@@ -11,6 +11,8 @@
 namespace quantnum {
 
 class boost_date {
+    typedef boost::gregorian::date date_type;
+    typedef boost::gregorian::date_duration duration_type;
 public:
     boost_date(): date_(boost::gregorian::day_clock::local_day()) {}
     boost_date(int year, int month, int day): date_(year, month, day) {}
@@ -19,8 +21,12 @@ public:
     int month() const { return date_.month(); }
     int year() const { return date_.year(); }
 
+    boost_date operator+(int days) const { return boost_date(date_ + duration_type((long) days)); }
+    boost_date operator-(int days) const { return boost_date(date_ - duration_type((long) days)); }
+
     friend std::ostream& operator<<(std::ostream& out, const boost_date& date);
 private:
+    explicit boost_date(const date_type& date): date_(date) {}
     boost::gregorian::date date_;
 };
 
--- test/quantnum/time/DatesTest.cpp
+++ test/quantnum/time/DatesTest.cpp
@@ -8,8 +8,29 @@
 
 using namespace quantnum;
 
+namespace {
+
+void testDatesAddition(const local_date& actual, int shift, const local_date& expected) {
+    local_date lagged = actual + shift;
+
+    ASSERT_EQ(expected.day(), lagged.day());
+    ASSERT_EQ(expected.month(), lagged.month());
+    ASSERT_EQ(expected.year(), lagged.year());
+}
+
+void testDatesSubtraction(const local_date& actual, int shift, const local_date& expected) {
+    local_date lagged = actual - shift;
+
+    ASSERT_EQ(expected.day(), lagged.day());
+    ASSERT_EQ(expected.month(), lagged.month());
+    ASSERT_EQ(expected.year(), lagged.year());
+}
+
+}
+
 TEST(Dates, CreateSimpleDate) { // NOLINT
     local_date date(2018, 12, 21);
+
     ASSERT_EQ(21, date.day());
     ASSERT_EQ(12, date.month());
     ASSERT_EQ(2018, date.year());
@@ -27,4 +48,14 @@ TEST(Dates, CurrentDate) { // NOLINT
     ASSERT_EQ(tm_time.tm_mday, date.day());
     ASSERT_EQ(tm_time.tm_mon + 1, date.month());
     ASSERT_EQ(tm_time.tm_year + 1900, date.year());
+}
+
+TEST(Dates, Addition) { // NOLINT
+    testDatesAddition(local_date(2019, 1, 12), 2, local_date(2019, 1, 14));
+    testDatesAddition(local_date(2018, 12, 25), 7, local_date(2019, 1, 1));
+}
+
+TEST(Dates, Subtraction) { // NOLINT
+    testDatesSubtraction(local_date(2019, 1, 12), 2, local_date(2019, 1, 10));
+    testDatesSubtraction(local_date(2019, 1, 5), 7, local_date(2018, 12, 29));
 }
